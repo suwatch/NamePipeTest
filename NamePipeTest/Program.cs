2e1e0fbd7d0f69a30d2a87406a8b0a36efaa159a
Suwath Ch <suwatch@microsoft.com>
2018-10-25 10:57:13 -0700

    initial commit

---
+++ NamePipeTest/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NamePipeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WcfNamedPipe.Run(args);
+            Program.Run(args);
+        }
+
+        static void Run(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                var nServers = 5;
+                while (true)
+                {
+                    var server = new NamedPipeServerStream("PipesOfPiece", PipeDirection.InOut, nServers, 
+                        PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+                    Console.Write("Waiing for client ... ");
+                    server.WaitForConnection();
+                    Console.WriteLine("connected");
+                    StartServer(server);
+                }
+            }
+            else
+            {
+                //Client
+                var client = new NamedPipeClientStream("PipesOfPiece");
+                client.Connect();
+                StreamReader reader = new StreamReader(client);
+                StreamWriter writer = new StreamWriter(client);
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (String.IsNullOrEmpty(input)) break;
+                    writer.WriteLine(input);
+                    writer.Flush();
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+        }
+
+        static void StartServer(NamedPipeServerStream server)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(server))
+                    using (StreamWriter writer = new StreamWriter(server))
+                    {
+                        while (true)
+                        {
+                            var line = reader.ReadLine();
+                            if (line == null)
+                                break;
+                            Console.WriteLine("Received: " + line);
+                            var sent = string.Join(string.Empty, line.Reverse());
+                            Console.WriteLine("Sent: " + sent);
+                            writer.WriteLine(sent);
+                            writer.Flush();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                Console.WriteLine("Exited!");
+            });
+        }
+    }
+}
---
+++ NamePipeTest/WcfNamedPipe.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace NamePipeTest
+{
+    class WcfNamedPipe
+    {
+        const string BaseAddress = "net.pipe://localhost/HelloService";
+
+        public static void Run(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "server")
+            {
+                ServiceHost serviceHost = new ServiceHost(typeof(HelloService), new Uri(BaseAddress));
+                NetNamedPipeBinding binding = new NetNamedPipeBinding { MaxReceivedMessageSize = 2147483647 };
+                serviceHost.AddServiceEndpoint(typeof(IHelloService), binding, BaseAddress);
+                serviceHost.Open();
+
+                Console.WriteLine("Service started. Available in following endpoints");
+                foreach (var serviceEndpoint in serviceHost.Description.Endpoints)
+                {
+                    Console.WriteLine(serviceEndpoint.ListenUri.AbsoluteUri);
+                }
+
+                Console.WriteLine("Press enter to exit ..");
+                Console.ReadLine();
+                serviceHost.Close();
+            }
+            else
+            {
+                var proxy = new HelloServiceProxy();
+                Console.WriteLine(proxy.Echo(args.Length > 0 ? string.Join(" ", args) : "Hello World!"));
+                proxy.Close();
+            }
+        }
+
+        [ServiceContract]
+        public interface IHelloService
+        {
+            [OperationContract]
+            string Echo(string msg);
+        }
+
+        public class HelloService : IHelloService
+        {
+            public string Echo(string msg)
+            {
+                // simply echo with upper case
+                return msg.ToUpper();
+            }
+        }
+
+        public class HelloServiceProxy : ClientBase<IHelloService>
+        {
+            public HelloServiceProxy()
+                : base(new ServiceEndpoint(ContractDescription.GetContract(typeof(IHelloService)),
+                    new NetNamedPipeBinding(), new EndpointAddress(BaseAddress)))
+            {
+            }
+
+            public string Echo(string msg)
+            {
+                return Channel.Echo(msg);
+            }
+        }
+    }
+}
